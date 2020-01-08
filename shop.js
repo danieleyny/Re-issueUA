80a2ccd3edcb8953ed2a43307f63aa67ffd90624
danieleyny <danieleyny@gmail.com>
2020-01-08 15:09:43 -0500

    update to shop page - clickable and fixed folders

---
+++ shop.js
@@ -0,0 +1,151 @@
+/* When the user clicks on the button, 
+                    toggle between hiding and showing the dropdown content */
+                    function myFunction() {
+                        document.getElementById("myDropdown").classList.toggle("show");
+                      }
+                      
+                      // Close the dropdown if the user clicks outside of it
+                      window.onclick = function(event) {
+                        if (!event.target.matches('.dropbtn')) {
+                          var dropdowns = document.getElementsByClassName("dropdown-content");
+                          var i;
+                          for (i = 0; i < dropdowns.length; i++) {
+                            var openDropdown = dropdowns[i];
+                            if (openDropdown.classList.contains('show')) {
+                              openDropdown.classList.remove('show');
+                            }
+                          }
+                        }
+                      }
+   /* When the user clicks on the button, 
+                      toggle between hiding and showing the dropdown content */
+                      function myFunction1() {
+                        document.getElementById("myDropdown1").classList.toggle("show");
+                      }
+                      
+                      // Close the dropdown if the user clicks outside of it
+                      window.onclick = function(event) {
+                        if (!event.target.matches('.dropbtn1')) {
+                          var dropdowns1 = document.getElementsByClassName("dropdown-content1");
+                          var i;
+                          for (i = 0; i < dropdowns1.length; i++) {
+                            var openDropdown1 = dropdowns1[i];
+                            if (openDropdown1.classList.contains('show')) {
+                              openDropdown1.classList.remove('show');
+                            }
+                          }
+                        }
+                      }
+  
+  
+  
+  
+                       /* When the user clicks on the button, 
+                      toggle between hiding and showing the dropdown content */
+                      function myFunction2() {
+                        document.getElementById("myDropdown2").classList.toggle("show");
+                      }
+                      
+                      // Close the dropdown if the user clicks outside of it
+                      window.onclick = function(event) {
+                        if (!event.target.matches('.dropbtn2')) {
+                          var dropdowns2 = document.getElementsByClassName("dropdown-content2");
+                          var i;
+                          for (i = 0; i < dropdowns2.length; i++) {
+                            var openDropdown2 = dropdowns2[i];
+                            if (openDropdown2.classList.contains('show')) {
+                              openDropdown2.classList.remove('show');
+                            }
+                          }
+                        }
+                      }
+  
+  
+  
+  
+                       /* When the user clicks on the button, 
+                      toggle between hiding and showing the dropdown content */
+                      function myFunction3() {
+                        document.getElementById("myDropdown3").classList.toggle("show");
+                      }
+                      
+                      // Close the dropdown if the user clicks outside of it
+                      window.onclick = function(event) {
+                        if (!event.target.matches('.dropbtn3')) {
+                          var dropdowns3 = document.getElementsByClassName("dropdown-content3");
+                          var i;
+                          for (i = 0; i < dropdowns3.length; i++) {
+                            var openDropdown3 = dropdowns3[i];
+                            if (openDropdown3.classList.contains('show')) {
+                              openDropdown3.classList.remove('show');
+                            }
+                          }
+                        }
+                      }
+  
+  
+  
+  
+  
+  
+                      /* When the user clicks on the button, 
+                      toggle between hiding and showing the dropdown content */
+                      function myFunction4() {
+                        document.getElementById("myDropdown4").classList.toggle("show");
+                      }
+                      
+                      // Close the dropdown if the user clicks outside of it
+                      window.onclick = function(event) {
+                        if (!event.target.matches('.dropbtn4')) {
+                          var dropdowns4 = document.getElementsByClassName("dropdown-content4");
+                          var i;
+                          for (i = 0; i < dropdowns4.length; i++) {
+                            var openDropdown4 = dropdowns4[i];
+                            if (openDropdown4.classList.contains('show')) {
+                              openDropdown4.classList.remove('show');
+                            }
+                          }
+                        }
+                      }
+  
+  
+  
+  
+  
+  
+  
+  
+                       /* When the user clicks on the button, 
+                      toggle between hiding and showing the dropdown content */
+                      function myFunction5() {
+                        document.getElementById("myDropdown5").classList.toggle("show");
+                      }
+                      
+                      // Close the dropdown if the user clicks outside of it
+                      window.onclick = function(event) {
+                        if (!event.target.matches('.dropbtn5')) {
+                          var dropdowns5 = document.getElementsByClassName("dropdown-content5");
+                          var i;
+                          for (i = 0; i < dropdowns5.length; i++) {
+                            var openDropdown5 = dropdowns5[i];
+                            if (openDropdown5.classList.contains('show')) {
+                              openDropdown5.classList.remove('show');
+                            }
+                          }
+                        }
+                      }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
